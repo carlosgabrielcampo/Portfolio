379c85390dba3f6ef533fe59cc6d3cfb87fa3021
Carlos Gabriel Campo Roman <carlosgabrielcampo@gmail.com>
2024-01-16 10:39:07 +1100

    about me page created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import About from './components/About';
 import Home from './components/Home';
 import Layout from './components/Layout';
 import { Route, Routes } from 'react-router-dom';
@@ -9,6 +10,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+        </Route>
+        <Route path="/about" element={<Layout />}>
+          <Route index element={<About />} />
         </Route>
       </Routes>
     </>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -20,23 +20,25 @@ const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
+                <h1>
                 <span className={letterClass}>H</span>
-                <span className={letterClass}>i,</span>
+                <span className={`${letterClass} _12`}>i,</span>
                 <br />
-                <span className={letterClass}>I</span>
-                <span className={letterClass}>'m</span>
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
                 <img src={LogoTitle} alt="developer" />
                 <AnimatedLetters 
                     letterClass={letterClass}
                     strArray={nameArray}
-                    idx={11}
+                    idx={15}
                 />
                 <br />
                 <AnimatedLetters 
                     letterClass={letterClass}
                     strArray={jobArray}
-                    idx={25}
+                    idx={22}
                 />
+                </h1>
                 <h2>Full-Stack Developer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
