ea0853f92559630b02ab31cf19401e718bb14cea
Carlos Gabriel Campo Roman <carlosgabrielcampo@gmail.com>
2024-01-16 09:56:49 +1100

    creatong logo animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,33 +1,46 @@
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = "arlos Roman".split('')
     const jobArray = "web developer".split('')
 
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //       setLetterClass('text-animate-hover')
+    //     }, 4000)
+    //   }, [])
+
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm 
+                <span className={letterClass}>H</span>
+                <span className={letterClass}>i,</span>
+                <br />
+                <span className={letterClass}>I</span>
+                <span className={letterClass}>'m</span>
                 <img src={LogoTitle} alt="developer" />
                 <AnimatedLetters 
                     letterClass={letterClass}
                     strArray={nameArray}
-                    idx={15}
+                    idx={11}
                 />
                 <br />
                 <AnimatedLetters 
                     letterClass={letterClass}
                     strArray={jobArray}
-                    idx={15}
-                />                </h1>
+                    idx={25}
+                />
                 <h2>Full-Stack Developer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
     )
 }
